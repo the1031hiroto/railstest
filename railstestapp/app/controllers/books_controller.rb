3f4bd10c3f3822cb85ddd873b4395d64a97216ad
hiroto soda <crazy_hiroto445@yahoo.co.jp>
2018-10-23 11:11:55 +0900

    delete json
    delete json

--- railstestapp/app/controllers/books_controller.rb
+++ railstestapp/app/controllers/books_controller.rb
@@ -3,7 +3,6 @@ class BooksController < ApplicationController
   before_action :check_logined, only: [:index, :show, :edit, :update, :destroy]
 
   # GET /books
-  # GET /books.json
   def index
     @books = Book.all
     @user_name = cookies[:username]
@@ -12,7 +11,6 @@ class BooksController < ApplicationController
   end
 
   # GET /books/1
-  # GET /books/1.json
   def show
     @user_id = cookies[:user_id]
   end
@@ -29,7 +27,6 @@ class BooksController < ApplicationController
   end
 
   # POST /books
-  # POST /books.json
   def create
     @book = Book.new(book_params)
     @book.user_id = cookies[:user_id]
@@ -37,16 +34,13 @@ class BooksController < ApplicationController
     respond_to do |format|
       if @book.save
         format.html { redirect_to @book, notice: 'Book was successfully created.' }
-        format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /books/1
-  # PATCH/PUT /books/1.json
   def update
     respond_to do |format|
       if @book.update(book_params)
@@ -60,7 +54,6 @@ class BooksController < ApplicationController
   end
 
   # DELETE /books/1
-  # DELETE /books/1.json
   def destroy
     @book.destroy
     respond_to do |format|
